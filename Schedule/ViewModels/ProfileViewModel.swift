590c0dff4b85c0ff6cc3c39e34f50e0184539dd9
Ahmet Can Öztürk <ahmetcanozturk53@gmail.com>
2024-08-06 19:35:30 +0300

    Updated Home Page

--- Schedule/ViewModels/ProfileViewModel.swift
+++ Schedule/ViewModels/ProfileViewModel.swift
@@ -1,14 +1,29 @@
 //
-//  ProfileViewModel.swift
-//  Schedule
+// ProfileViewModel.swift
+// Schedule
 //
-//  Created by Ahmet Can Öztürk on 6.08.2024.
+// Created by Ahmet Can Öztürk on 6.08.2024.
 //
 
 import Foundation
+import Combine
 
-class ProfileViewModel: ObservableObject{
-    
-     init(){}
-    
+class ProfileViewModel: ObservableObject {
+    @Published var isUserLoggedIn: Bool = true
+    @Published var userToken: String = ""
+
+    func logout() {
+        UserService.shared.logout(token: userToken) { [weak self] result in
+            switch result {
+            case .success:
+                DispatchQueue.main.async {
+                    self?.isUserLoggedIn = false
+                    self?.userToken = ""
+                    UserDefaults.standard.removeObject(forKey: "userToken")
+                }
+            case .failure(let error):
+                print("Logout failed: \(error.localizedDescription)")
+            }
+        }
+    }
 }
